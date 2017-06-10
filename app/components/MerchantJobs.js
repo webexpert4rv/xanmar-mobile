83911cd37d2ba36a18b730a33f0e156f8d7190ad
Landon <landonmr@yahoo.com>
2017-06-10 12:17:02 -0400

    Removed hardcoded provider id

--- app/components/MerchantJobs.js
+++ app/components/MerchantJobs.js
@@ -37,7 +37,7 @@ export default class MerchantJobs extends Component {
   }
 
   fetchData() {
-    fetch(format('{}/api/provider/jobs/{}', constants.BASSE_URL,110))
+    fetch(format('{}/api/provider/jobs/{}', constants.BASSE_URL, this.getUserId()))
       .then(response => response.json())
       .then((responseData) => {
         const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
